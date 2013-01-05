002174acd6571865447116a23b519a6eb91e1d6e
Jamie Wright <jamie@brilliantfantastic.com>
2013-01-05 14:06:02 -0500

    2-CreateFeature

---
+++ GitHubFantasyLeague.Specs/StepDefinitions/GithubSteps.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace GitHubFantasyLeague.Specs.StepDefinitions
+{
+    [Binding]
+    public class GithubSteps
+    {
+        [Given(@"I have a valid Github username")]
+        public void GivenIHaveAValidGithubUsername()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"I enter the Github username")]
+        public void WhenIEnterTheGithubUsername()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"I should see the total score")]
+        public void ThenIShouldSeeTheTotalScore()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+    }
+}
---
+++ GitHubFantasyLeague.Specs/StepDefinitions/NavigationSteps.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace GitHubFantasyLeague.Specs.StepDefinitions
+{
+    [Binding]
+    public class NavigationSteps
+    {
+        [Given(@"I am on the home page")]
+        public void GivenIAmOnTheHomePage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+    }
+}
